268e40575ed5622006d804bc802ffee0d71c8097
rajeshpoojari39 <rajeshpoojari39@gmail.com>
2022-04-03 19:23:07 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Section from "./components/Section";
+import Card from "./components/Card";
+import Footer from "./components/Footer";
+import { data } from "./data/productData";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const cartCountHandler = (isAdded) => {
+    isAdded ? setCount(count - 1) : setCount(count + 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar cartCount={count} />
+      <Header />
+      <Section>
+        {data.map((productData) => {
+          return (
+            <Card
+              key={productData.id}
+              productData={productData}
+              onAddToCart={cartCountHandler}
+            />
+          );
+        })}
+      </Section>
+      <Footer />
     </div>
   );
 }
